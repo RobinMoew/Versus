7dc8107bdd313c471a975bdb3d1e19ef897ed858
RobinMoew <robin.marcot1992@gmail.com>
2019-09-23 14:36:54 +0200

    js

--- addPerso.js
+++ addPerso.js
@@ -1,25 +1,25 @@
-let url = $('#url').val();
-let id = $('#id').val();
-let nom = $('#nom').val();
-let pv =  $('#pv').val();
-let pa = $('#pa').val();
-
 $('#valid_perso').click(function() {
-    $.ajax({
-        url:"addPerso.php",
-        type:"POST",
-        data:{
-            url:url,
-            id:id,
-            pv:pv,
-            pa:pa
-        },
-    success : function success(result) {
-        alert(result);
-    },
-    error : function erreur(error) {
-        console.log(error);
-    },
-    })
+  let url = $('#url').val();
+  let id = $('#id').val();
+  let nom = $('#nom').val();
+  let pv = $('#pv').val();
+  let pa = $('#pa').val();
 
+  $.ajax({
+    url: 'http://localhost/Versus/addPerso.php',
+    type: 'POST',
+    data: {
+      url: url,
+      id: id,
+      nom: nom,
+      pv: pv,
+      pa: pa
+    },
+    success: function success(result) {
+      alert(result);
+    },
+    error: function erreur(error) {
+      console.log(error);
+    }
+  });
 });
