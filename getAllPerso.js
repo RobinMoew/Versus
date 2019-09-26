0c4e85c49d9e6dfe60a1213476fad478ce7a2781
RobinMoew <robin.marcot1992@gmail.com>
2019-09-26 12:31:41 +0200

    select perso new

--- getAllPerso.js
+++ getAllPerso.js
@@ -17,13 +17,61 @@ $(document).ready(() => {
         `);
       }
 
+      let one_clickedFighter;
+      let one_last_clicked;
+      let one_idFighter;
+      let one_last_idFighter;
+
+      let second_clickedFighter;
+      let second_last_clicked;
+      let second_idFighter;
+      let second_last_idFighter;
+
+      let selected = false;
+
       $('.fighter').click((event) => {
-        let clickedFighter = event.currentTarget;
-        let idFighter = clickedFighter.getAttribute('id');
-        if ($('#id_self').val() == '') {
-          $('#id_self').val(idFighter);
+        if (!selected) {
+          one_clickedFighter = event.currentTarget;
+
+          if (one_clickedFighter.classList.contains('one_selected')) {
+            one_clickedFighter.classList.add('one_selected');
+          } else {
+            selected = true;
+            one_clickedFighter = event.currentTarget;
+            one_clickedFighter.classList.toggle('one_selected');
+            one_idFighter = one_clickedFighter.getAttribute('id');
+            $('#id_self').val(one_idFighter);
+
+            if (one_last_idFighter == undefined) {
+              one_last_idFighter = one_idFighter;
+              one_last_clicked = one_clickedFighter;
+            } else if (one_idFighter != one_last_idFighter) {
+              one_last_clicked.classList.toggle('one_selected');
+              one_last_idFighter = one_idFighter;
+              one_last_clicked = one_clickedFighter;
+            }
+          }
         } else {
-          $('#id_versus').val(idFighter);
+          second_clickedFighter = event.currentTarget;
+
+          if (second_clickedFighter.classList.contains('second_selected')) {
+            second_clickedFighter.classList.add('second_selected');
+          } else {
+            selected = false;
+            second_clickedFighter = event.currentTarget;
+            second_clickedFighter.classList.toggle('second_selected');
+            second_idFighter = second_clickedFighter.getAttribute('id');
+            $('#id_versus').val(second_idFighter);
+
+            if (second_last_idFighter == undefined) {
+              second_last_idFighter = second_idFighter;
+              second_last_clicked = second_clickedFighter;
+            } else if (second_idFighter != second_last_idFighter) {
+              second_last_clicked.classList.toggle('second_selected');
+              second_last_idFighter = second_idFighter;
+              second_last_clicked = second_clickedFighter;
+            }
+          }
         }
       });
     },
