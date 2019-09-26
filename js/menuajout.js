e290135bb4dbd0caeab9a45c0b34892cbd233c8a
momo13910 <mohamed.yahyaoui10@sfr.fr>
2019-09-26 14:28:02 +0200

    add_perso

--- js/menuajout.js
+++ js/menuajout.js
@@ -5,12 +5,14 @@ document.getElementById("menu").style.transform="scale(3)";
    document.getElementById("plus").style.transform="rotate(45deg)"; 
    document.getElementById("plus").style.left="28px"; 
    document.getElementById("plus").style.bottom="-4px"; 
+   document.getElementById("add").style.display="none";
     i=1;
   }
   else{   document.getElementById("menu").style.transform="scale(0)"; 
    document.getElementById("plus").style.transform="rotate(0deg)"; 
    document.getElementById("plus").style.left="4px"; 
    document.getElementById("plus").style.bottom="0px"; 
+   document.getElementById("add").style.display="block";
     i=0;
   }
 }
