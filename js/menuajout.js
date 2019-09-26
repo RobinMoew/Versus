e68521639b72bc2d50e621775e2498908e3c65d8
momo13910 <mohamed.yahyaoui10@sfr.fr>
2019-09-26 14:10:26 +0200

    ajoutMenu

--- js/menuajout.js
+++ js/menuajout.js
@@ -3,7 +3,7 @@ function expand(){
   if(i==0){
 document.getElementById("menu").style.transform="scale(3)"; 
    document.getElementById("plus").style.transform="rotate(45deg)"; 
-   document.getElementById("plus").style.left="25px"; 
+   document.getElementById("plus").style.left="28px"; 
    document.getElementById("plus").style.bottom="-4px"; 
     i=1;
   }
